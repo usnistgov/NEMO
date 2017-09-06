ccb7bf5e9c64fee568d936c6521aac8497e0ca86
dylan-klomparens <dylan.klomparens@nist.gov>
2017-09-06 16:00:37 -0400

    Fixed bugs in the API. Some API pages were not loading because of dependancy upgrades. Changed API date/time filtering to accept ISO 8601 formatting.

--- NEMO/filters.py
+++ NEMO/filters.py
@@ -1,31 +1,32 @@
-from django_filters import FilterSet, DateTimeFilter, BooleanFilter
+from django_filters import FilterSet, IsoDateTimeFilter, BooleanFilter
+from django_filters.widgets import BooleanWidget
 
 from NEMO.models import Reservation, UsageEvent, AreaAccessRecord
 
 
 class ReservationFilter(FilterSet):
-	start_gte = DateTimeFilter('start', lookup_type='gte')
-	start_lt = DateTimeFilter('start', lookup_type='lt')
-	missed = BooleanFilter('missed')
+	start_gte = IsoDateTimeFilter('start', lookup_expr='gte')
+	start_lt = IsoDateTimeFilter('start', lookup_expr='lt')
+	missed = BooleanFilter('missed', widget=BooleanWidget())
 
 	class Meta:
 		model = Reservation
-		fields = ['start']
+		fields = []
 
 
 class UsageEventFilter(FilterSet):
-	start_gte = DateTimeFilter('start', lookup_type='gte')
-	start_lt = DateTimeFilter('start', lookup_type='lt')
+	start_gte = IsoDateTimeFilter('start', lookup_expr='gte')
+	start_lt = IsoDateTimeFilter('start', lookup_expr='lt')
 
 	class Meta:
 		model = UsageEvent
-		fields = ['start']
+		fields = []
 
 
 class AreaAccessRecordFilter(FilterSet):
-	start_gte = DateTimeFilter('start', lookup_type='gte')
-	start_lt = DateTimeFilter('start', lookup_type='lt')
+	start_gte = IsoDateTimeFilter('start', lookup_expr='gte')
+	start_lt = IsoDateTimeFilter('start', lookup_expr='lt')
 
 	class Meta:
 		model = AreaAccessRecord
-		fields = ['start']
+		fields = []
--- NEMO/serializers.py
+++ NEMO/serializers.py
@@ -3,19 +3,19 @@ from rest_framework import serializers
 from NEMO.models import User, Project, Account, Reservation, AreaAccessRecord, UsageEvent
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ('id', 'first_name', 'last_name', 'username')
 
 
-class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+class ProjectSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Project
 		fields = ('id', 'name', 'application_identifier', 'active')
 
 
-class AccountSerializer(serializers.HyperlinkedModelSerializer):
+class AccountSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Account
 		fields = ('id', 'name', 'active')
@@ -24,13 +24,16 @@ class AccountSerializer(serializers.HyperlinkedModelSerializer):
 class ReservationSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Reservation
+		fields = '__all__'
 
 
 class UsageEventSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = UsageEvent
+		fields = '__all__'
 
 
 class AreaAccessRecordSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = AreaAccessRecord
+		fields = '__all__'
