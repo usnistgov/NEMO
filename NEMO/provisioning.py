edcb328fe0d809cf0137e385bc0e068abe590881
dylan-klomparens <dylan.klomparens@nist.gov>
2018-01-29 10:40:30 -0500

    Added two commands to NEMO's provisioning script:  * query_public_key - prints to stdout the TLS public key of a given host/port.  * test_ldap_authentication - tests LDAPS authentication for a given (host, domain, username, password, public key).

--- NEMO/provisioning.py
+++ NEMO/provisioning.py
@@ -1,4 +1,7 @@
+from _ssl import CERT_REQUIRED, PROTOCOL_TLSv1_2
+from getpass import getpass
 from os import remove
+from ssl import get_server_certificate
 from subprocess import run
 from sys import argv
 from textwrap import dedent
@@ -9,12 +12,18 @@ from cryptography.hazmat.primitives.hashes import SHA256
 from cryptography.hazmat.primitives.serialization import Encoding, NoEncryption, PrivateFormat
 from cryptography.x509 import NameAttribute, NameOID, DNSName, CertificateSigningRequestBuilder, Name, SubjectAlternativeName
 from django.utils.crypto import get_random_string
+from ldap3 import Tls, Server, Connection, AUTO_BIND_TLS_BEFORE_BIND, SIMPLE
+from ldap3.core.exceptions import LDAPBindError, LDAPExceptionError
 
 
 def entry_point():
 	request = argv[1] if len(argv) == 2 else None
 	if request == 'generate_secret_key':
 		generate_secret_key()
+	elif request == 'query_public_key':
+		query_public_key()
+	elif request == 'test_ldap_authentication':
+		test_ldap_authentication()
 	elif request == 'generate_tls_keys':
 		generate_tls_keys()
 	elif request == 'install_systemd_service':
@@ -32,6 +41,11 @@ def entry_point():
 					Prints a randomly generated secret key, used for hashing user sessions,
 					cryptographic signing, and more. More information is available from the Django documentation:
 					https://docs.djangoproject.com/en/1.11/ref/settings/#std:setting-SECRET_KEY
+				
+				query_public_key -
+					Opens a TLS connection to another server and prints the public TLS key.
+					This is useful if you want to verify correct TLS configuration,
+					or pin a particular certificate for LDAPS authentication.
 				
 				generate_tls_keys -
 					Generates an RSA 4096 bit private key and certificate signing request. These can be used
@@ -68,6 +82,37 @@ def generate_secret_key():
 	Documentation available at https://docs.djangoproject.com/en/1.11/ref/settings/#std:setting-SECRET_KEY
 	"""
 	print(get_random_string(50, "abcdefghijklmnopqrstuvwxyz0123456789!@$%&=-+#(*_^)"))
+
+
+def query_public_key():
+	"""  """
+	print("Enter a hostname and port to query a server's public TLS key.")
+	print("Port 636 is commonly used for secure LDAP.")
+	print("Port 443 is commonly used for secure HTTP.")
+	hostname = input("Hostname = ")
+	port = input("Port = ")
+	certificate = get_server_certificate((hostname, port))
+	print(certificate)
+
+
+def test_ldap_authentication():
+	"""  """
+	hostname = input("Hostname = ")
+	domain = input("Domain = ")
+	username = input("Username = ")
+	password = getpass("Password = ")
+	certificate = input("Path to public key certificate = ")
+	try:
+		t = Tls(validate=CERT_REQUIRED, version=PROTOCOL_TLSv1_2, ca_certs_file=certificate)
+		s = Server(hostname, port=636, use_ssl=True, tls=t)
+		c = Connection(s, user='{}\\{}'.format(domain, username), password=password, auto_bind=AUTO_BIND_TLS_BEFORE_BIND, authentication=SIMPLE)
+		c.unbind()
+		# At this point the user successfully authenticated to at least one LDAP server.
+		print("Authentication successful!")
+	except LDAPBindError as e:
+		pass  # When this error is caught it means the username and password were invalid against the LDAP server.
+	except LDAPExceptionError as e:
+		print(e)
 
 
 def generate_tls_keys():
@@ -155,15 +200,20 @@ def install_systemd_service():
 	with open('/etc/systemd/system/nemo.service', 'w') as f:
 		f.write(dedent(service))
 
-	print("The systemd NEMO service was successfully installed")
+	print("The systemd NEMO service was successfully installed.")
 
 
 def uninstall_systemd_service():
 	""" Uninstalls NEMO as a systemd service """
 	remove('/etc/systemd/system/nemo.service')
-	print("The systemd NEMO service was successfully uninstalled")
+	print("The systemd NEMO service was successfully uninstalled.")
 
 
 def open_firewall_ports():
 	http_result = run(['firewall-cmd', '--zone=public', '--permanent', '--add-service=http'])
 	https_result = run(['firewall-cmd', '--zone=public', '--permanent', '--add-service=https'])
+	reload_result = run(['firewall-cmd', '--reload'])
+
+
+if __name__ == "__main__":
+	entry_point()
