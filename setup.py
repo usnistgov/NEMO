50b691d2d8b936d1f8ec596c9bd1082e84131906
dlk1 <dylan.klomparens@nist.gov>
2018-09-06 15:56:42 -0400

    Bump version for bug fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='NEMO',
-	version='1.2.0',
+	version='1.2.1',
 	python_requires='>=3.6',
 	packages=find_packages(exclude=['NEMO.tests']),
 	include_package_data=True,
