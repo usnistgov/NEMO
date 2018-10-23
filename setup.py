8c89716065f87706502be230bc811729f93545c4
dlk1 <dylan.klomparens@nist.gov>
2018-10-23 10:16:00 -0400

    Add occupancy list to Kiosk tablet page

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='NEMO',
-	version='1.5.0',
+	version='1.6.0',
 	python_requires='>=3.6',
 	packages=find_packages(exclude=['NEMO.tests']),
 	include_package_data=True,
