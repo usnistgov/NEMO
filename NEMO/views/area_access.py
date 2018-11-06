ecb7d75aca9d58c179d4a304f211d7f47f1d9399
dlk1 <dylan.klomparens@nist.gov>
2018-11-06 14:52:10 -0500

    Super-users can not customize NEMO to allow people to log themselves in to an access controlled area. This is designed to be a safeguard against tablet login failure.

--- NEMO/views/area_access.py
+++ NEMO/views/area_access.py
@@ -13,6 +13,7 @@ from django.views.decorators.http import require_POST, require_GET, require_http
 from NEMO.models import AreaAccessRecord, Project, User, Door, PhysicalAccessType, Area, PhysicalAccessLog
 from NEMO.tasks import postpone
 from NEMO.utilities import parse_start_and_end_date
+from views.customization import get_customization
 
 
 @staff_member_required(login_url=None)
@@ -314,3 +315,69 @@ def new_area_access_record(request):
 		record.save()
 		dictionary['success'] = '{} is now logged in to the {}.'.format(user, area.name.lower())
 		return render(request, 'area_access/new_area_access_record.html', dictionary)
+
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def self_log_in(request):
+	if not able_to_self_log_in_to_area(request.user):
+		return redirect(reverse('landing'))
+	dictionary = {
+		'projects': request.user.active_projects(),
+	}
+	areas = []
+	for access_level in request.user.physical_access_levels.all():
+		unavailable_resources = access_level.area.required_resources.filter(available=False)
+		if access_level.accessible() and not unavailable_resources:
+			areas.append(access_level.area)
+	dictionary['areas'] = areas
+	if request.method == 'GET':
+		return render(request, 'area_access/self_login.html', dictionary)
+	if request.method == 'POST':
+		try:
+			a = Area.objects.get(id=request.POST['area'])
+			p = Project.objects.get(id=request.POST['project'])
+			if a in dictionary['areas'] and p in dictionary['projects']:
+				AreaAccessRecord.objects.create(area=a, customer=request.user, project=p)
+		except:
+			pass
+		return redirect(reverse('landing'))
+
+
+def able_to_self_log_in_to_area(user):
+	# 'Self log in' must be enabled
+	if not get_customization('self_log_in') == 'enabled':
+		return False
+	# Check if the user is active
+	if not user.is_active:
+		return False
+	# Check if the user has a billable project
+	if user.active_project_count() < 1:
+		return False
+	# Check if the user is already in an area. If so, the /change_project/ URL can be used to change their project.
+	if user.in_area():
+		return False
+	# Check if the user has any physical access levels
+	if not user.physical_access_levels.all().exists():
+		return False
+	# Check if the user normally has access to this area at the current time
+	accessible_areas = []
+	for access_level in user.physical_access_levels.all():
+		if access_level.accessible():
+			accessible_areas.append(access_level.area)
+	if not accessible_areas:
+		return False
+	# Check that the user's physical access has not expired
+	if user.access_expiration is not None and user.access_expiration < date.today():
+		return False
+	# Staff are exempt from the remaining rule checks
+	if user.is_staff:
+		return True
+	# Users may not access an area if a required resource is unavailable,
+	# so return true if there exists at least one area they are able to log in to.
+	for area in accessible_areas:
+		unavailable_resources = area.required_resources.filter(available=False)
+		if not unavailable_resources:
+			return True
+	# No areas are accessible...
+	return False
