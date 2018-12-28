7abad7559ebc181c02c507d926a87608baed3752
mrampant <mathieu.rampant@gmail.com>
2018-12-28 12:08:26 -0500

    added email field to Users Rest API

--- NEMO/serializers.py
+++ NEMO/serializers.py
@@ -6,7 +6,7 @@ from NEMO.models import User, Project, Account, Reservation, AreaAccessRecord, U
 class UserSerializer(ModelSerializer):
 	class Meta:
 		model = User
-		fields = ('id', 'first_name', 'last_name', 'username', 'date_joined')
+		fields = ('id', 'first_name', 'last_name', 'username', 'email', 'date_joined')
 
 
 class ProjectSerializer(ModelSerializer):
