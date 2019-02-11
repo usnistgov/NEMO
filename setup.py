11e9bb121e7bfe4f3740c8e86e840c8e00ef0d80
mrampant <mathieu.rampant@gmail.com>
2019-02-11 11:21:02 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='NEMO',
-	version='1.10.0',
+	version='1.11.0',
 	python_requires='>=3.6',
 	packages=find_packages(exclude=['NEMO.tests']),
 	include_package_data=True,
