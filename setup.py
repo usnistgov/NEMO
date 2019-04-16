51868a7274052839282a4e332e9b6300add62202
Mathieu Rampant <mathieu.rampant@gmail.com>
2019-04-16 09:31:48 -0400

    bumped up bug fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='NEMO',
-	version='1.12.0',
+	version='1.12.1',
 	python_requires='>=3.6',
 	packages=find_packages(exclude=['NEMO.tests']),
 	include_package_data=True,
