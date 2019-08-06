eb1b6379f571815f584397ff1cd5ee4f02fb22be
mrampant <mathieu.rampant@gmail.com>
2019-08-06 08:15:26 -0400

    updated django framework version (security fix)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='NEMO',
-	version='1.14.3',
+	version='1.14.4',
 	python_requires='>=3.6',
 	packages=find_packages(exclude=['NEMO.tests']),
 	include_package_data=True,
@@ -25,7 +25,7 @@ setup(
 	],
 	install_requires=[
 		'cryptography==2.3.1',
-		'Django==1.11.22',
+		'Django==1.11.23',
 		'django-filter==1.1.0',
 		'djangorestframework==3.9.4',
 		'ldap3==2.5.1',
