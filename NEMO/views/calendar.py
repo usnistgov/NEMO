44942c2c4e8b7869355dbe256d80655374a3fcae
mrampant <mathieu.rampant@gmail.com>
2020-04-22 23:14:06 -0400

    - when making a reservation on desktop, staff don't need to choose a project (consistent with mobile)

--- NEMO/views/calendar.py
+++ NEMO/views/calendar.py
@@ -237,19 +237,20 @@ def create_reservation(request):
 
 	# If the user only has one project then associate it with the reservation.
 	# Otherwise, present a dialog box for the user to choose which project to associate.
-	active_projects = user.active_projects()
-	if len(active_projects) == 1:
-		new_reservation.project = active_projects[0]
-	else:
-		try:
-			new_reservation.project = Project.objects.get(id=request.POST['project_id'])
-		except:
-			return render(request, 'calendar/project_choice.html', {'active_projects': active_projects})
+	if not user.is_staff:
+		active_projects = user.active_projects()
+		if len(active_projects) == 1:
+			new_reservation.project = active_projects[0]
+		else:
+			try:
+				new_reservation.project = Project.objects.get(id=request.POST['project_id'])
+			except:
+				return render(request, 'calendar/project_choice.html', {'active_projects': active_projects})
 
-	# Make sure the user is actually enrolled on the project. We wouldn't want someone
-	# forging a request to reserve against a project they don't belong to.
-	if new_reservation.project not in new_reservation.user.active_projects():
-		return render(request, 'calendar/project_choice.html', {'active_projects': active_projects})
+		# Make sure the user is actually enrolled on the project. We wouldn't want someone
+		# forging a request to reserve against a project they don't belong to.
+		if new_reservation.project not in new_reservation.user.active_projects():
+			return render(request, 'calendar/project_choice.html', {'active_projects': active_projects})
 
 	configured = (request.POST.get('configured') == "true")
 	# If a reservation is requested and the tool does not require configuration...
