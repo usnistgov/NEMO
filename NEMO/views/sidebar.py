215690201fc89671c28195eb3a20a8c0b43518cc
mrampant <mathieu.rampant@gmail.com>
2020-06-04 17:54:55 -0400

    - added visual indication that area is unavailable due to required resource being unavailable

--- NEMO/views/sidebar.py
+++ NEMO/views/sidebar.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.decorators.http import require_GET
 
 from NEMO.decorators import disable_session_expiry_refresh
-from NEMO.models import Area
+from NEMO.models import Area, Resource
 from NEMO.views.status_dashboard import create_tool_summary
 
 
@@ -18,6 +18,7 @@ def refresh_sidebar_icons(request):
 
 def create_area_summary():
 	areas = Area.objects.filter(requires_reservation=True)
+	unavailable_resources = Resource.objects.filter(available=False).prefetch_related('dependent_areas')
 	result = {}
 	for area in areas:
 		result[area.id] = {
@@ -27,7 +28,11 @@ def create_area_summary():
 			'warning_capacity': area.warning_capacity(),
 			'danger_capacity': area.danger_capacity(),
 			'occupancy_count': area.occupancy_count(),
+			'required_resource_is_unavailable': False,
 		}
+	for resource in unavailable_resources:
+		for area in resource.dependent_areas.all():
+			result[area.id]['required_resource_is_unavailable'] = True
 	area_summary = list(result.values())
 	area_summary.sort(key=lambda x: x['name'])
 	return area_summary
