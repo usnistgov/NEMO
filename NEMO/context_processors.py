cd6a405ce129b754f8ed5abe79e8eb5825d53dae
mrampant <mathieu.rampant@gmail.com>
2020-06-10 22:43:47 -0400

    - added context_processor to hide certain menu items when tools/areas don't exists

--- NEMO/context_processors.py
+++ NEMO/context_processors.py
@@ -1,3 +1,4 @@
+from NEMO.models import Tool, Area, PhysicalAccessLevel
 from NEMO.views.customization import get_customization
 
 def show_logout_button(request):
@@ -25,8 +26,18 @@ def base_context(request):
 		site_title = get_customization("site_title")
 	except:
 		site_title = ""
+	try:
+		tools_exist = Tool.objects.filter(visible=True).exist()
+	except:
+		tools_exist = False
+	try:
+		areas_exist = Area.objects.filter(requires_reservation=True).exist() and PhysicalAccessLevel.objects.exists()
+	except:
+		areas_exist = False
 	return {
 		"facility_name": facility_name,
 		"site_title": site_title,
 		"device": request.device,
+		"tools_exist": tools_exist,
+		"areas_exist": areas_exist,
 	}
