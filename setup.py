fe6cff418708f662a76d627ffb0cc7129641cf9e
mrampant <mathieu.rampant@gmail.com>
2021-06-24 11:26:37 -0400

    - updated flex fields library version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
 		'Django==2.2.24',
 		'django-filter==2.4.0',
 		'djangorestframework==3.12.4',
-		'drf-flex-fields==0.9.0',
+		'drf-flex-fields==0.9.1',
 		'ldap3==2.9',
 		'python-dateutil==2.8.1',
 		'requests==2.25.1',
