48664d020c317c17921c01c73f4cd39568dc892e
mrampant <mathieu.rampant@gmail.com>
2021-08-02 14:44:54 -0400

    - setting up 3.13.0.dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='NEMO',
-	version='3.12.0',
+	version='3.13.0.dev',
 	python_requires='>=3.6',
 	packages=find_packages(exclude=['NEMO.tests','NEMO.tests.*']),
 	include_package_data=True,
