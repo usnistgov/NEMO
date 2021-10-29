b0b5f0fb028fb654e3bab13fe188556c6160e674
mrampant <mathieu.rampant@gmail.com>
2021-10-29 15:49:24 -0400

    - consolidating migrations

--- NEMO/migrations/0035_version_3_14_0.py
+++ NEMO/migrations/0035_version_3_14_0.py
@@ -27,4 +27,24 @@ class Migration(migrations.Migration):
             ],
             options={'ordering': ['-end_time']},
         ),
+        migrations.AddField(
+            model_name='user',
+            name='is_facility_manager',
+            field=models.BooleanField(default=False, help_text='Designates this user as facility manager. Facility managers receive updates on all reported problems in the facility and can also review access requests.', verbose_name='facility manager'),
+        ),
+        migrations.AlterField(
+            model_name='user',
+            name='is_staff',
+            field=models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff'),
+        ),
+        migrations.AlterField(
+            model_name='user',
+            name='is_superuser',
+            field=models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='administrator'),
+        ),
+        migrations.AlterField(
+            model_name='user',
+            name='is_technician',
+            field=models.BooleanField(default=False, help_text='Specifies how to bill staff time for this user. When checked, customers are billed at technician rates.', verbose_name='technician'),
+        ),
     ]
