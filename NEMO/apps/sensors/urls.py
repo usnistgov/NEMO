4056ded81a371b58618074a3fc01584838d17ab0
mrampant <mathieu.rampant@gmail.com>
2022-04-22 10:59:29 -0400

    - added tabs in sensor data page - added button to export sensor data

--- NEMO/apps/sensors/urls.py
+++ NEMO/apps/sensors/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from NEMO.apps.sensors import views
 
 urlpatterns = [
 	path("sensors/", views.sensors, name="sensors"),
 	path("sensor_details/<int:sensor_id>/", views.sensor_details, name="sensor_details"),
+	re_path(r"sensor_details/(?P<sensor_id>\d+)/(?P<tab>chart|data)/$", views.sensor_details, name="sensor_details"),
 	path("sensor_chart_data/<int:sensor_id>/", views.sensor_chart_data, name="sensor_chart_data"),
+	path("export_sensor_data/<int:sensor_id>/", views.export_sensor_data, name="export_sensor_data"),
 	path("manage_sensor_data/", views.manage_sensor_data, name="manage_sensor_data"),
 ]
--- NEMO/apps/sensors/views.py
+++ NEMO/apps/sensors/views.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 from math import floor
 
 from django.contrib.auth.decorators import login_required, permission_required
@@ -6,11 +6,18 @@ from django.db.models import QuerySet
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
+from django.utils.text import slugify
 from django.views.decorators.http import require_GET
 
 from NEMO.apps.sensors.models import Sensor, SensorData
 from NEMO.decorators import postpone, staff_member_required
-from NEMO.utilities import beginning_of_the_day, extract_times, format_datetime
+from NEMO.utilities import (
+	BasicDisplayTable,
+	beginning_of_the_day,
+	export_format_datetime,
+	extract_times,
+	format_datetime,
+)
 
 
 @staff_member_required
@@ -23,11 +30,34 @@ def sensors(request):
 
 @staff_member_required
 @require_GET
-def sensor_details(request, sensor_id):
+def sensor_details(request, sensor_id, tab: str = None):
 	sensor = get_object_or_404(Sensor, pk=sensor_id)
-	csv_export = bool(request.POST.get("csv", False))
 	chart_step = int(request.GET.get("chart_step", 1))
-	return render(request, "sensors/sensor_data.html", {"sensor": sensor, "chart_step": chart_step})
+	return render(
+		request, "sensors/sensor_data.html", {"tab": tab or "chart", "sensor": sensor, "chart_step": chart_step}
+	)
+
+
+@staff_member_required
+@require_GET
+def export_sensor_data(request, sensor_id):
+	sensor = get_object_or_404(Sensor, pk=sensor_id)
+	sensor_data, start, end = get_sensor_data(request, sensor)
+	table_result = BasicDisplayTable()
+	table_result.add_header(("date", "Date"))
+	table_result.add_header(("value", "Value"))
+	table_result.add_header(("display_value", "Display value"))
+	for data_point in sensor_data:
+		table_result.add_row({
+			"date": format_datetime(data_point.created_date, "SHORT_DATETIME_FORMAT"),
+			"value": data_point.value,
+			"display_value": data_point.display_value()
+		})
+	response = table_result.to_csv()
+	sensor_name = slugify(sensor.name).replace("-", "_")
+	filename = f"{sensor_name}_data_{export_format_datetime(start)}_to_{export_format_datetime(end)}.csv"
+	response["Content-Disposition"] = f'attachment; filename="{filename}"'
+	return response
 
 
 @staff_member_required
@@ -36,21 +66,21 @@ def sensor_chart_data(request, sensor_id):
 	sensor = get_object_or_404(Sensor, pk=sensor_id)
 	labels = []
 	data = []
-	sensor_data = get_sensor_data(request, sensor).order_by("created_date")
+	sensor_data = get_sensor_data(request, sensor)[0].order_by("created_date")
 	for data_point in sensor_data:
 		labels.append(format_datetime(data_point.created_date, "m/d/Y H:i:s"))
 		data.append(data_point.value)
 	return JsonResponse(data={"labels": labels, "data": data})
 
 
-def get_sensor_data(request, sensor) -> QuerySet:
+def get_sensor_data(request, sensor) -> (QuerySet, datetime, datetime):
 	start, end = extract_times(request.POST, start_required=False, end_required=False)
 	sensor_data = SensorData.objects.filter(sensor=sensor)
 	if not start:
 		start = timezone.now() - timedelta(days=1)
 	if not end:
 		end = timezone.now()
-	return sensor_data.filter(created_date__gte=start, created_date__lte=end)
+	return sensor_data.filter(created_date__gte=start, created_date__lte=end), start, end
 
 
 @login_required
