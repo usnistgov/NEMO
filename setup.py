235e3b06fa58436cd88598fdc48799fe61e160bf
mrampant <mathieu.rampant@gmail.com>
2022-09-02 11:29:23 -0400

    - starting 4.3.0 dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
 	name='NEMO',
-	version='4.2.0',
+	version='4.3.0.dev',
 	python_requires='>=3.7',
 	packages=find_packages(exclude=['NEMO.tests','NEMO.tests.*']),
 	include_package_data=True,
