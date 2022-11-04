e7887a2c51cf0404b57f98ef74844b597747ad5c
mrampant <mathieu.rampant@gmail.com>
2022-11-04 13:16:30 -0400

    - fixed usage data history being all lowercase after first letter

--- NEMO/tests/test_misc.py
+++ NEMO/tests/test_misc.py
@@ -6,8 +6,9 @@ from NEMO.utilities import capitalize
 class MiscTests(TestCase):
 
 	def test_capitalize(self):
-		self.assertEqual(capitalize(""), "")
 		self.assertEqual(capitalize(None), None)
+		self.assertEqual(capitalize(""), "")
+		self.assertEqual(capitalize("j"), "J")
 		self.assertEqual(capitalize("John Doe"), "John Doe")
 		self.assertEqual(capitalize("john Doe"), "John Doe")
 		self.assertEqual(capitalize("john doe"), "John doe")
