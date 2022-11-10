2b5c156ed416a41202df1b914858475baab45d5a
mrampant <mathieu.rampant@gmail.com>
2022-11-10 07:43:22 -0500

    - added SDS sorting by hazard and optimized SDS loading query

--- NEMO/views/safety_data_sheets.py
+++ NEMO/views/safety_data_sheets.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.db.models.functions import Lower
+from django.db.models import Case, When
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
 
@@ -9,7 +9,25 @@ from NEMO.models import Chemical, ChemicalHazard
 @login_required
 @require_GET
 def safety_data_sheets(request):
-	chemicals = Chemical.objects.all().order_by(Lower("name"))
+	chemicals = Chemical.objects.all().prefetch_related("hazards").order_by()
 	hazards = ChemicalHazard.objects.all()
 
-	return render(request, "safety_data_sheets.html", {"chemicals": chemicals, "hazards": hazards})
+	for hazard in hazards:
+		chemicals = chemicals.annotate(
+			**{f"hazard_{hazard.id}": Case(When(hazards__in=[hazard.id], then=True), default=False)}
+		)
+
+	order_by = request.GET.get("o", "name")
+	reverse = order_by.startswith("-")
+	order = order_by[1:] if reverse else order_by
+	chemicals = list(set(chemicals))
+	if order == "name":
+		chemicals.sort(key=lambda x: x.name.lower(), reverse=reverse)
+	elif order.startswith("hazard_"):
+		hazard_id = int(order[7:])
+		chemicals.sort(key=lambda x: x.name.lower())
+		chemicals.sort(key=lambda x: hazard_id in [h.id for h in x.hazards.all()], reverse=not reverse)
+
+	dictionary = {"chemicals": chemicals, "hazards": hazards, "order_by": order_by}
+
+	return render(request, "safety_data_sheets.html", dictionary)
