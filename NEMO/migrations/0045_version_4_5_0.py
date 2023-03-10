088d7d9d4f8f88d3350c0d61a7a68d16e648aa23
mrampant <mathieu.rampant@gmail.com>
2023-03-10 14:02:02 -0500

    - consolidating migrations

--- NEMO/migrations/0045_version_4_5_0.py
+++ NEMO/migrations/0045_version_4_5_0.py
@@ -138,5 +138,55 @@ class Migration(migrations.Migration):
             name='visible',
             field=models.BooleanField(default=True, help_text='Specifies whether this staff member should be displayed on the staff status page.'),
         ),
+        migrations.AlterModelOptions(
+            name='accounttype',
+            options={'ordering': ['display_order', 'name']},
+        ),
+        migrations.AlterModelOptions(
+            name='projectdiscipline',
+            options={'ordering': ['display_order', 'name']},
+        ),
+        migrations.AlterModelOptions(
+            name='usertype',
+            options={'ordering': ['display_order', 'name']},
+        ),
+        migrations.AddField(
+            model_name='accounttype',
+            name='display_order',
+            field=models.IntegerField(default=0, help_text='The display order is used to sort these items. The lowest value category is displayed first.'),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='projectdiscipline',
+            name='display_order',
+            field=models.IntegerField(default=0, help_text='The display order is used to sort these items. The lowest value category is displayed first.'),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='usertype',
+            name='display_order',
+            field=models.IntegerField(default=0, help_text='The display order is used to sort these items. The lowest value category is displayed first.'),
+            preserve_default=False,
+        ),
+        migrations.AlterField(
+            model_name='accounttype',
+            name='name',
+            field=models.CharField(help_text='The unique name for this item', max_length=200, unique=True),
+        ),
+        migrations.AlterField(
+            model_name='usertype',
+            name='name',
+            field=models.CharField(help_text='The unique name for this item', max_length=200, unique=True),
+        ),
+        migrations.AlterField(
+            model_name='interlock',
+            name='unit_id',
+            field=models.PositiveIntegerField(blank=True, null=True, verbose_name='Multiplier/Unit id'),
+        ),
+        migrations.AlterField(
+            model_name='safetyissue',
+            name='location',
+            field=models.CharField(blank=True, max_length=200, null=True),
+        ),
     ]
 
