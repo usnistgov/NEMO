20fa222f61f25754e313ed94eb22edff7d3297e5
mrampant <mathieu.rampant@nist.gov>
2023-05-13 13:01:11 -0400

    - fixed issue with related field in csv api import

--- NEMO/parsers.py
+++ NEMO/parsers.py
@@ -1,4 +1,6 @@
 import csv
+import json
+from typing import Iterable, List, Union
 
 from rest_framework.exceptions import ParseError
 from rest_framework.parsers import BaseParser
@@ -26,8 +28,19 @@ class CSVParser(BaseParser):
 			# Clean them up
 			headers = [c.strip() for c in header_row] if (header_row is not None) else None
 			# Create data dict structure
-			data = [dict(zip(headers, row)) for row in rows]
+			data = [dict(zip(headers, self.check_for_related_fields(row))) for row in rows]
 			# If only one record return it as json object, otherwise return our list of json object
 			return data[0] if len(data) == 1 else data
 		except Exception as exc:
 			raise ParseError("CSV parse error - %s" % str(exc))
+
+	def check_for_related_fields(self, row: Iterable) -> Iterable:
+		return [self.to_json(item) for item in row]
+
+	def to_json(self, item: str) -> Union[str, List]:
+		if item and item.startswith("["):
+			try:
+				return json.loads(item)
+			except:
+				pass
+		return item
