88c9b07fe99623464e399a82984c6894baf0ffd5
mrampant <mathieu.rampant@gmail.com>
2023-05-21 16:37:27 -0400

    - added convenient method to wait for an element to be added to the dom

--- NEMO/static/nemo.js
+++ NEMO/static/nemo.js
@@ -729,3 +729,29 @@ function sort_elements(list_element, selector, sort_function)
 		}
 	}
 }
+
+function wait_for_element(selector)
+{
+    return new Promise(resolve =>
+	{
+        if (document.querySelector(selector))
+		{
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations =>
+		{
+            if (document.querySelector(selector))
+			{
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body,
+		{
+			childList: true,
+			subtree: true
+		});
+    });
+}
