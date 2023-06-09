62452d937bbbda867f5263dcc8982cee68038bbe
mrampant <mathieu.rampant@gmail.com>
2023-06-09 16:47:18 -0400

    - removed unused validation

--- NEMO/apps/contracts/customization.py
+++ NEMO/apps/contracts/customization.py
@@ -1,5 +1,3 @@
-from django.core.validators import validate_comma_separated_integer_list
-
 from NEMO.decorators import customization
 from NEMO.views.customization import CustomizationBase
 
@@ -12,8 +10,3 @@ class ContractsCustomization(CustomizationBase):
         "contracts_view_accounting_officer": "",
         "contracts_contractors_default_empty_label": "Credit card order",
     }
-
-    def validate(self, name, value):
-        if name in ["contracts_renewal_reminder_days", "contracts_contractors_reminder_days"] and value:
-            # Check that we have an integer or a list of integers
-            validate_comma_separated_integer_list(value)
