8c24cecd9d4bc2b207393f4a801fb1e5aedb3dcd
mrampant <mathieu.rampant@gmail.com>
2023-06-13 20:18:11 -0400

    - fixed badge reader not behaving properly without recording key

--- NEMO/static/badge_reader.js
+++ NEMO/static/badge_reader.js
@@ -2,14 +2,15 @@ BadgeReader = function (on_success, send_key, record_key)
 {
 
     $(window).keypress(on_keypress).keydown(on_keydown);
-    var record_badge_number = false;
-    var badge_number = "";
+    let record_badge_number = false;
+    let badge_number = "";
 
     // Note that keydown and keyup provide a code indicating which key is pressed, while keypress indicates which character was entered. For example, a lowercase "a" will be reported as 65 by keydown and keyup, but as 97 by keypress. An uppercase "A" is reported as 65 by all events. Because of this distinction, when catching special keystrokes such as F2, .keydown() or .keyup() is a better choice.
 
     function on_keypress(event)
     {
-        if (record_key && record_badge_number || !record_key)
+        let skip_recording_key = event.key === send_key && send_key !== record_key;
+        if (!skip_recording_key && (record_key && record_badge_number || !record_key))
         {
             badge_number += String.fromCharCode(event.which);
             $("#badge_number").html(badge_number);
