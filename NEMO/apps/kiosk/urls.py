39e74df7c45b6e5ba424d5d81884d2bdd5b0a291
r-xyz <100710244+r-xyz@users.noreply.github.com>
2023-11-08 11:48:16 +0100

    Added usage `parameter` to kiosk template.
    It calls to status_dashboard.status_dashboard(interest='tools') and renders the response.

--- NEMO/apps/kiosk/urls.py
+++ NEMO/apps/kiosk/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path, re_path
 
 from NEMO.apps.kiosk import views
-from NEMO.views import area_access
+from NEMO.views import area_access, status_dashboard
 
 urlpatterns = [
     # Tablet kiosk
@@ -10,6 +10,7 @@ urlpatterns = [
         include(
             [
                 path("occupancy/", area_access.occupancy, name="kiosk_occupancy"),
+                path("kiosk_usage/", status_dashboard.status_dashboard, name="kiosk_usage"),
                 path("enable_tool/", views.enable_tool, name="enable_tool_from_kiosk"),
                 path("disable_tool/", views.disable_tool, name="disable_tool_from_kiosk"),
                 path("reserve_tool/", views.reserve_tool, name="reserve_tool_from_kiosk"),
