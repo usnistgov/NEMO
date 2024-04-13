34e896cb2bf874f4b27d29e657a75b37f301de88
mrampant <mathieu.rampant@nist.gov>
2024-04-13 16:11:17 -0400

    - added missing systemd service for waitlist mode

--- NEMO/management/commands/check_and_update_wait_list.py
+++ NEMO/management/commands/check_and_update_wait_list.py
@@ -5,7 +5,7 @@ from NEMO.views.timed_services import do_check_and_update_wait_list
 
 class Command(BaseCommand):
     help = (
-        "Run every minute to update tool wait list."
+        "Run every minute to check and update tool wait list."
         "Wait list notification email and slot time to expiration have to be set in customizations for this to work correctly."
     )
 
