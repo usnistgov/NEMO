fce7d8cf47ee7a6bc66ea3d8b074e22ca4f4d248
Alyssa Sheffler <asheffle@uci.edu>
2024-05-24 20:42:52 -0700

    View User Profile (#229)
    * added customization to allow users to view their profile by clicking on their name in the navigation bar

--- NEMO/views/users.py
+++ NEMO/views/users.py
@@ -481,6 +481,27 @@ def user_preferences(request):
     return render(request, "users/preferences.html", dictionary)
 
 
+@login_required
+@require_GET
+def view_user(request, user_id):
+    if UserCustomization.get_bool("user_allow_profile_view"):
+        user = get_object_or_404(User, pk=user_id)
+    
+        if request.user.id != user_id:
+            return HttpResponseBadRequest("You are not allowed to view this user's profile")
+    
+        dictionary = {
+            "user": user,
+            "projects": Project.objects.filter(active=True, account__active=True),
+            "tool_qualifications": user.qualifications.all(),
+            "groups": user.groups.all(),
+        }
+    
+        return render(request, "users/view_user.html", dictionary)
+    else:
+        return HttpResponseBadRequest("You cannot view this page")
+
+
 def readonly_users(request):
     # Only user office and facility managers can edit user information
     user: User = request.user
