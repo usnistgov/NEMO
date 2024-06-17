d48e53b059bee2c51ab44cb7caa126d6c8d17a59
Aaron Buckles <abuckles@uci.edu>
2024-06-17 09:47:58 -0700

    Change expiration for staff assistance requests

--- NEMO/views/notifications.py
+++ NEMO/views/notifications.py
@@ -87,8 +87,7 @@ def create_buddy_request_notification(buddy_request: BuddyRequest):
 
 def create_staff_assistance_request_notification(staff_assistance_request: StaffAssistanceRequest):
     users: List[User] = User.objects.filter(is_active=True, is_staff=True).exclude(id=staff_assistance_request.user_id)
-    created_at = staff_assistance_request.creation_time
-    expiration = datetime(9999, 1, 1)
+    expiration = datetime(staff_assistance_request.creation_time.year + 100, 1, 1)
     for u in users:
         if u.get_preferences().display_new_buddy_request_notification:
             Notification.objects.update_or_create(
