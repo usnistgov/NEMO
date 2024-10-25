854cab3f64d91c3ae5680a67bf9c3fce62971ad4
mrampant <mathieu.rampant@nist.gov>
2024-10-25 08:46:35 -0400

    - added new GUNICORN environment variables to customize its configuration when using the docker container. this should allow more flexibility for workers and threads - the options are: GUNICORN_WORKER_CLASS, GUNICORN_WORKER_COUNT, GUNICORN_THREAD_COUNT, GUNICORN_KEEPALIVE_SECONDS, GUNICORN_CAPTURE_OUTPUT (defaults are set to previous values)

--- gunicorn_configuration.py
+++ gunicorn_configuration.py
@@ -1,14 +1,16 @@
 # This is the default Gunicorn configuration file for the NEMO Docker image.
 # It is mounted inside the image at /etc/gunicorn_configuration.py, and you can override it by
 # mounting your own configuration file to this location or providing command line arguments to Gunicorn.
-
+import os
 from multiprocessing import cpu_count
 
+from NEMO.utilities import strtobool
+
 bind = "0.0.0.0:8000"
-worker_class = "gthread"
+worker_class = os.getenv("GUNICORN_WORKER_CLASS", "gthread") or "gthread"
 # The following value was decided based on the Gunicorn documentation and configuration example:
 # http://docs.gunicorn.org/en/stable/configure.html#configuration-file
-workers = min(cpu_count() * 2 + 1, 9)
-threads = 8
-keepalive = 300
-capture_output = True
+workers = int(os.getenv("GUNICORN_WORKER_COUNT", min(cpu_count() * 2 + 1, 9)) or min(cpu_count() * 2 + 1, 9))
+threads = int(os.getenv("GUNICORN_THREAD_COUNT", "8") or "8")
+keepalive = int(os.getenv("GUNICORN_KEEPALIVE_SECONDS", "300") or "300")
+capture_output = bool(strtobool(os.getenv("GUNICORN_CAPTURE_OUTPUT", "True") or "True"))
