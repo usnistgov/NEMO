53cbc0854ff70155173204bceceae99578ef106f
mrampant <mathieu.rampant@gmail.com>
2024-11-04 10:09:27 -0500

    - fixed tests

--- NEMO/tests/test_formats.py
+++ NEMO/tests/test_formats.py
@@ -77,8 +77,15 @@ class FormatTestCase(TestCase):
 
     def test_duration_with_off_schedule_weekends(self):
         # 6-10 am local starting on a Tuesday (Sep 17 2024 is a Tuesday)
-        start = (
-            timezone.now().astimezone().replace(year=2024, month=9, day=17, hour=6, minute=0, second=0, microsecond=0)
+        start = timezone.now().replace(
+            year=2024,
+            month=9,
+            day=17,
+            hour=6,
+            minute=0,
+            second=0,
+            microsecond=0,
+            tzinfo=timezone.get_current_timezone(),
         )
         end = start + timedelta(hours=4) + timedelta(days=7)
         duration = end - start
@@ -109,8 +116,15 @@ class FormatTestCase(TestCase):
 
     def test_duration_with_off_schedule_weekdays_normal(self):
         # 6-10 am local on a Tuesday (Sep 17 2024 is a Tuesday)
-        start = (
-            timezone.now().astimezone().replace(year=2024, month=9, day=17, hour=6, minute=0, second=0, microsecond=0)
+        start = timezone.now().replace(
+            year=2024,
+            month=9,
+            day=17,
+            hour=6,
+            minute=0,
+            second=0,
+            microsecond=0,
+            tzinfo=timezone.get_current_timezone(),
         )
         end = start + timedelta(hours=4)
         duration = end - start
@@ -182,8 +196,15 @@ class FormatTestCase(TestCase):
 
     def test_duration_with_off_schedule_weekdays_reverse(self):
         # 6pm-10 am local on a Tuesday (Sep 17 2024 is a Tuesday)
-        start = (
-            timezone.now().astimezone().replace(year=2024, month=9, day=17, hour=18, minute=0, second=0, microsecond=0)
+        start = timezone.now().replace(
+            year=2024,
+            month=9,
+            day=17,
+            hour=18,
+            minute=0,
+            second=0,
+            microsecond=0,
+            tzinfo=timezone.get_current_timezone(),
         )
         end = start + timedelta(hours=16)
         duration = end - start
