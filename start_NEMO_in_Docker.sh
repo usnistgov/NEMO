bbd1fc475c2d3d603dd4c5153d2b60c664e024de
mrampant <mathieu.rampant@nist.gov>
2025-02-07 17:55:31 -0500

    Revert "- replaced direct package installation command with eval for extra pip packages"
    This reverts commit a369545980f313309d9d891b897906c4a3305187.

--- start_NEMO_in_Docker.sh
+++ start_NEMO_in_Docker.sh
@@ -5,7 +5,7 @@ set -e
 
 if [[ -n "$NEMO_EXTRA_PIP_PACKAGES" ]]; then
     echo "Installing additional Python packages: $NEMO_EXTRA_PIP_PACKAGES"
-    eval "python3 -m pip install $NEMO_EXTRA_PIP_PACKAGES"
+    python3 -m pip install $NEMO_EXTRA_PIP_PACKAGES
 else
     echo "No additional Python packages to install."
 fi
