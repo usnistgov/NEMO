145b9c7d5d79042fadb9d2fa4752f36adeeac678
r-xyz <100710244+r-xyz@users.noreply.github.com>
2025-04-16 01:21:05 +0200

    Run container image as non-root user by default.
    After installing additional pip packages, the container will adjust required permissions and run both django-admin and gunicorn commands as user `nemo:nemo`.
    UID and GID are 963 by default, but can be set using Docker Environment variables `PUID` and `GUID`, without need to recompile the image.

--- start_NEMO_in_Docker.sh
+++ start_NEMO_in_Docker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Exit if any of following commands fails
 set -e
 
@@ -10,11 +9,37 @@ else
     echo "No additional Python packages to install."
 fi
 
+# Set the PUID and PGID environment variables
+PUID=${PUID:-963}
+PGID=${PGID:-963}
+# Change the user and group IDs
+if [ "$PGID" -eq 0 ]; then
+    # If PGID is 0, use the root group
+    NEMO_GROUP="root"
+else
+    NEMO_GROUP="nemo"
+    groupmod -g "$PGID" "$NEMO_GROUP"
+fi
+if [ "$PUID" -eq 0 ]; then
+    # If PUID is 0, use the root user
+    NEMO_USER="root"
+else
+    NEMO_USER="nemo"
+    usermod -u "$PUID" "$NEMO_USER"
+fi
+
+# Change the ownership of the application directory
+if [ "$PUID" -ne 0 ]; then
+  chown -R nemo:nemo /nemo
+  chown -R root:nemo /etc/gunicorn_configuration.py
+fi
+echo "Running NEMO as user '${NEMO_USER}' (uid: ${PUID}), primary group '${NEMO_GROUP}' (gid: ${PGID})"
+
 # Collect static files
-django-admin collectstatic --no-input --clear
+su "${NEMO_USER}" -g "${NEMO_GROUP}" -c "django-admin collectstatic --no-input --clear"
 
 # Run migrations to create or update the database
-django-admin migrate
+su ${NEMO_USER} -c "django-admin migrate"
 
 # Run NEMO
-exec gunicorn --config=/etc/gunicorn_configuration.py NEMO.wsgi:application
+su ${NEMO_USER} -c "exec gunicorn --config=/etc/gunicorn_configuration.py NEMO.wsgi:application"
