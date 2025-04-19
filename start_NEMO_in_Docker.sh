4e067838ab0a2521043d8211528d411f180ff399
r-xyz <100710244+r-xyz@users.noreply.github.com>
2025-04-19 23:38:57 +0200

    Simplified docker non-root startup script.

--- start_NEMO_in_Docker.sh
+++ start_NEMO_in_Docker.sh
@@ -12,34 +12,24 @@ fi
 # Set the PUID and PGID environment variables
 PUID=${PUID:-963}
 PGID=${PGID:-963}
-# Change the user and group IDs
-if [ "$PGID" -eq 0 ]; then
-    # If PGID is 0, use the root group
-    NEMO_GROUP="root"
+# If PUID is not 0 (root)
+if [ -n "$PUID" ]; then
+    # Change the user and group IDs
+    usermod -u "$PUID" -g "$PGID" nemo
+    # Change the ownership of the application directory
+    chown -R nemo:nemo /nemo
+    chown -R root:nemo /etc/gunicorn_configuration.py
+    NEMO_USER=nemo
 else
-    NEMO_GROUP="nemo"
-    groupmod -g "$PGID" "$NEMO_GROUP"
+    NEMO_USER=root
 fi
-if [ "$PUID" -eq 0 ]; then
-    # If PUID is 0, use the root user
-    NEMO_USER="root"
-else
-    NEMO_USER="nemo"
-    usermod -u "$PUID" "$NEMO_USER"
-fi
-
-# Change the ownership of the application directory
-if [ "$PUID" -ne 0 ]; then
-  chown -R nemo:nemo /nemo
-  chown -R root:nemo /etc/gunicorn_configuration.py
-fi
-echo "Running NEMO as user '${NEMO_USER}' (uid: ${PUID}), primary group '${NEMO_GROUP}' (gid: ${PGID})"
+echo "Running NEMO as user '$(id $NEMO_USER)'"
 
 # Collect static files
-su "${NEMO_USER}" -g "${NEMO_GROUP}" -c "django-admin collectstatic --no-input --clear"
+su "${NEMO_USER}" -c "django-admin collectstatic --no-input --clear"
 
 # Run migrations to create or update the database
-su ${NEMO_USER} -c "django-admin migrate"
+su "${NEMO_USER}" -c "django-admin migrate"
 
 # Run NEMO
-su ${NEMO_USER} -c "exec gunicorn --config=/etc/gunicorn_configuration.py NEMO.wsgi:application"
+su "${NEMO_USER}" -c "exec gunicorn --config=/etc/gunicorn_configuration.py NEMO.wsgi:application"
