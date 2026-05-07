df18aae4b4a41a36dde0bc0dc66142cdc5261ed5
mrampant <mathieu.rampant@nist.gov>
2026-05-07 10:50:50 -0400

    - suppressed RuntimeWarnings in admin site initialization, safe queryset evaluation, and customization initialization

--- NEMO/views/customization.py
+++ NEMO/views/customization.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import time
+import warnings
 from abc import ABC
 from datetime import date, datetime
 from logging import getLogger
@@ -672,7 +673,10 @@ class RecurringChargesCustomization(CustomizationBase):
 
     def __init__(self, key, title):
         super().__init__(key, title)
-        self.update_title()
+        # Ignore warning when updating title during initialization
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=RuntimeWarning)
+            self.update_title()
 
     def context(self) -> Dict:
         # Override to add list of consumable categories
