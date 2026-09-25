0ad3519fe7388759326da781881e58e3296a7b37
Neil Coplin <coplin.7@osu.edu>
2026-09-25 15:33:26 -0400

    Settings check; override output
    - Adding check for EMAIL_OVERRIDE to be present in settings if the backend is used, throw an error if it is not set or empty.
    - Output the original to/cc/bcc addresses at the top of the email message prior to overriding them.

--- NEMO/backends.py
+++ NEMO/backends.py
@@ -3,10 +3,20 @@ from django.core.mail.backends.smtp import EmailBackend
 
 class OverrideSmtpEmailBackend(EmailBackend):
     def send_messages(self, email_messages):
+        emailOverride = getattr(settings, "EMAIL_OVERRIDE", [])
+        # Throw an error if EMAIL_OVERRIDE was not defined or set to an empty list
+        if not emailOverride or emailOverride is None or emailOverride.count == 0:
+            raise NameError('Email override failed: No recipients in EMAIL_OVERRIDE in setting.')
         for message in email_messages:
-            # todo: Add the original to/cc/bcc fields were at top of message for easier debugging
-            # Override recipients list globally
-            message.to = [getattr(settings, "EMAIL_OVERRIDE", [])]
+            # Add original recipients to email, processing backwards so order is to, cc, bcc in the final string.
+            for bccAddress in message.bcc:
+                message.body = message.body.replace("<body>", f"<body>Original Bcc: {bccAddress}<br />")
+            for ccAddress in message.cc:
+                message.body = message.body.replace("<body>", f"<body>Original Cc: {ccAddress}<br />")
+            for toAddress in message.to:
+                message.body = message.body.replace("<body>", f"<body>Original To: {toAddress}<br />")
+            # Override recipients list for all outgoing emails
+            message.to = [emailOverride]
             message.cc = []
             message.bcc = []
         return super().send_messages(email_messages)
