945228e6a4cab264078e9b617047f5b7d2dac21e
mrampant <mathieu.rampant@gmail.com>
2026-09-25 17:20:40 -0400

    - small fixes to email backend

--- NEMO/backends.py
+++ NEMO/backends.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.core.mail.backends.smtp import EmailBackend
 
+
 class OverrideSmtpEmailBackend(EmailBackend):
     def send_messages(self, email_messages):
-        emailOverride = getattr(settings, "EMAIL_OVERRIDE", [])
+        email_override = getattr(settings, "EMAIL_OVERRIDE", [])
         # Throw an error if EMAIL_OVERRIDE was not defined or set to an empty list
-        if not emailOverride or emailOverride is None or emailOverride.count == 0:
-            raise NameError('Email override failed: No recipients in EMAIL_OVERRIDE in setting.')
+        if not email_override:
+            raise NameError("Email override failed: No recipients in EMAIL_OVERRIDE in setting.")
         for message in email_messages:
             # Add original recipients to email, processing backwards so order is to, cc, bcc in the final string.
             for bccAddress in message.bcc:
@@ -16,7 +17,7 @@ class OverrideSmtpEmailBackend(EmailBackend):
             for toAddress in message.to:
                 message.body = message.body.replace("<body>", f"<body>Original To: {toAddress}<br />")
             # Override recipients list for all outgoing emails
-            message.to = [emailOverride]
+            message.to = [email_override]
             message.cc = []
             message.bcc = []
         return super().send_messages(email_messages)
